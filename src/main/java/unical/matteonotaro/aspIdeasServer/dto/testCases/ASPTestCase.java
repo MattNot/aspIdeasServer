aa4141a72f7e7c187e5d2686e198e950ada19aa8
MattNot <matteoatnotaro@gmail.com>
2020-06-10 00:28:16 +0200

    Working on constraint

--- src/main/java/unical/matteonotaro/aspIdeasServer/dto/testCases/ASPTestCase.java
+++ src/main/java/unical/matteonotaro/aspIdeasServer/dto/testCases/ASPTestCase.java
@@ -22,9 +22,9 @@ public class ASPTestCase {
             for (String rule : block.getRules())
                 builder.append(rule);
         }
-        for (String program : programFiles) {
-            builder.append(program);
-        }
+//        for (String program : programFiles) {
+//            builder.append(program);
+//        }
         return builder.toString();
     }
 }
